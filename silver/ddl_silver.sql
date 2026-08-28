1d18a8c44d445580bd825b09646dceca6a423d0c
Nasser <kiti.nasser@gmail.com>
2026-08-28 15:54:25 +0300

    refactor(silver): Add (again) crm_cust_info to ddl

--- silver/ddl_silver.sql
+++ silver/ddl_silver.sql
@@ -8,8 +8,8 @@ if they already exist.
 Run this script to re-define the DDL structure of 'silver' Tables
 ===============================================================================
  */
-IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
-DROP TABLE silver.crm_prd_info;
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE silver.crm_cust_info;
 
 CREATE TABLE
     silver.crm_cust_info (
